6851197c815e1a1577cd9f6d26bfb710418d804d
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-19 23:04:35 +0330

    Make the calculator functional, only works with two numbers for now

--- script.js
+++ script.js
@@ -3,75 +3,95 @@ function subtract(a, b) { return a - b }
 function multiply(a, b) { return a * b }
 function divide(a, b) { return a / b }
 
-function operate(operator, a, b) {
-    switch (operator) {
-        case "+":
-            return add(a, b)
+// function operate(operator, a, b) {
+//     switch (operator) {
+//         case "+":
+//             return add(a, b)
 
-        case "-":
-            return subtract(a, b)
+//         case "-":
+//             return subtract(a, b)
 
-        case "*":
-            return multiply(a, b)
+//         case "*":
+//             return multiply(a, b)
 
-        case "/":
-            return divide(a, b)
-    }
-}
+//         case "/":
+//             return divide(a, b)
+//     }
+// }
 
 const display = document.querySelector(".display")
 const clearBtn = document.querySelector(".clear")
 const allDigits = document.querySelectorAll(".digit")
 
-// we wanna check weather displayNumber already exists or not
-let displayNumber;
+let displayNumber = null
 
 allDigits.forEach(digit => {
     digit.addEventListener("click", (e) => {
         if (!displayNumber) {
-            // we check that if display number isn't created yet (it's the first click)
-            // Create a new <p> element for the number
+            // we wanna check weather displayNumber already exists or not
             displayNumber = document.createElement("p");
             displayNumber.textContent = e.target.textContent;
             displayNumber.classList.add("number");
             display.appendChild(displayNumber);
-        } else {
-            // Update the text content for the next clicks
+        }
+        else {
             displayNumber.textContent += e.target.textContent;
         }
-    });
-});
+    })
+})
 
-clearBtn.addEventListener("click", () => {
-    // Clear the display and reset the displayNumber variable
-    display.innerHTML = "";
-    displayNumber = null;
-});
-
-// TO DO
-// MAKE IT SO WHEN AN OPERATOR BUTTON IS CLICKED
 const allOperatorBtn = document.querySelectorAll(".op")
 const multiplyBtn = document.querySelector(".multiply")
 const minusBtn = document.querySelector(".minus")
 const plusBtn = document.querySelector(".plus")
 const divideBtn = document.querySelector(".divide")
+const equalsBtn = document.querySelector(".equals")
 
+let firstNum;
+let secondNum;
+let operateorBtnTarget;
 
 allOperatorBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        switch (e.target) {
-            case multiplyBtn:
-                let firstNum = document.querySelectorAll("p")
-                firstNum = firstNum.textContent
-                console.log(firstNum)
-        }
+        operateorBtnTarget = e.target
+        firstNum = document.querySelector(".number")
+        firstNum = +firstNum.textContent
+        display.removeChild(displayNumber)
+        displayNumber = null
     })
 })
 
+let result;
+equalsBtn.addEventListener("click", () => {
+    // evaluate the second number
+    secondNum = document.querySelector(".number")
+    secondNum = +secondNum.textContent
 
-// THE NUMBERS THAT ALREADY EXIST
-// GET SENT AS THE FIRST NUMBER TO THE REQUIRED OPERATOR FUNCTION
-// THE DISPLAY IS CLEARED
-// THE SECOND NUMBER IS INPUTTED AND IS THE SECOND NUMBER IN THE FUNCTION
-// AND WHEN THE EQUALS IS CLICKED
-// THE VALUE THAT THE FUNCTION RETURNED IS DISPLAYED
+    switch (operateorBtnTarget) {
+        case multiplyBtn:
+            result = multiply(firstNum, secondNum)
+            break;
+
+        case minusBtn:
+            result = subtract(firstNum, secondNum)
+            break;
+
+        case plusBtn:
+            result = add(firstNum, secondNum)
+            break;
+
+        case divideBtn:
+            result = divide(firstNum, secondNum)
+            break;
+    }
+    displayNumber.textContent = result
+})
+
+clearBtn.addEventListener("click", () => {
+    if (displayNumber !== null) {
+        display.removeChild(displayNumber);
+        displayNumber = null;
+        firstNum = null
+        secondNum = null
+    }
+})
