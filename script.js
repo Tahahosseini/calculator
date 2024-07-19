c8e33d9e0cc21327596c1c56723d41a1ba6d69e3
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-19 21:57:19 +0330

    Edit the event listener so when digits are clicked they are added to the existing p tag.
    I did this so that we can have a unified number inside a p tag instead of a bunch of p tags each containing a number so we can do operations on the number.

--- script.js
+++ script.js
@@ -23,24 +23,52 @@ const display = document.querySelector(".display")
 const clearBtn = document.querySelector(".clear")
 const allDigits = document.querySelectorAll(".digit")
 
+// we wanna check weather displayNumber already exists or not
+let displayNumber;
+
 allDigits.forEach(digit => {
     digit.addEventListener("click", (e) => {
-        let displayNumber = document.createElement("p");
-        displayNumber.textContent = e.target.textContent;
-        displayNumber.classList.add("number");
-        display.appendChild(displayNumber);
+        if (!displayNumber) {
+            // we check that if display number isn't created yet (it's the first click)
+            // Create a new <p> element for the number
+            displayNumber = document.createElement("p");
+            displayNumber.textContent = e.target.textContent;
+            displayNumber.classList.add("number");
+            display.appendChild(displayNumber);
+        } else {
+            // Update the text content for the next clicks
+            displayNumber.textContent += e.target.textContent;
+        }
     });
 });
 
 clearBtn.addEventListener("click", () => {
-    let allNumbers = display.querySelectorAll(".number")
-    allNumbers.forEach((number) => {
-        display.removeChild(number)
-    })
-})
+    // Clear the display and reset the displayNumber variable
+    display.innerHTML = "";
+    displayNumber = null;
+});
 
 // TO DO
 // MAKE IT SO WHEN AN OPERATOR BUTTON IS CLICKED
+const allOperatorBtn = document.querySelectorAll(".op")
+const multiplyBtn = document.querySelector(".multiply")
+const minusBtn = document.querySelector(".minus")
+const plusBtn = document.querySelector(".plus")
+const divideBtn = document.querySelector(".divide")
+
+
+allOperatorBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        switch (e.target) {
+            case multiplyBtn:
+                let firstNum = document.querySelectorAll("p")
+                firstNum = firstNum.textContent
+                console.log(firstNum)
+        }
+    })
+})
+
+
 // THE NUMBERS THAT ALREADY EXIST
 // GET SENT AS THE FIRST NUMBER TO THE REQUIRED OPERATOR FUNCTION
 // THE DISPLAY IS CLEARED
